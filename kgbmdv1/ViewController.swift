8006b4627afe50f454dc49db8c73bcb7d0e07043
Konrad Biernacki <kgbier@gmail.com>
2019-11-30 18:02:56 +1100

    Initial Commit

---
+++ kgbmdv1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  kgbmdv1
+//
+//  Created by Konrad Biernacki on 30/11/19.
+//  Copyright © 2019 Konrad Biernacki. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
