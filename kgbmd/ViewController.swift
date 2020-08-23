45275160e233f70d6010179472dd48e0c912e5c9
Konrad Biernacki <kgbier@gmail.com>
2020-08-23 11:19:26 +1000

    Remove main storyboard

--- kgbmd/ViewController.swift
+++ kgbmd/ViewController.swift
@@ -1,15 +1,53 @@
 import UIKit
 
+class RootNavigationController: UINavigationController {
+
+    override init(rootViewController: UIViewController) {
+        super.init(rootViewController: rootViewController)
+        navigationBar.prefersLargeTitles = true
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
 class ViewController: UIViewController {
+
+    let testPosterView = PosterView()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
 
-        ImdbRepo.getMovieHotListPosters().sink { (it) in
+        view.backgroundColor = .systemBackground
+
+        navigationItem.title = "Hot films"
+
+        view.addSubview(testPosterView)
+
+        testPosterView.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            testPosterView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            testPosterView.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            testPosterView.widthAnchor.constraint(equalToConstant: 96),
+            testPosterView.heightAnchor.constraint(equalToConstant: 128)
+        ])
+
+        _ = ImdbRepo.getMovieHotListPosters().sink { (it) in
             print(it)
         }
     }
 
 }
 
+class PosterView: UIView {
+    override init(frame: CGRect = .zero) {
+        super.init(frame: frame)
+        layer.cornerRadius = 4
+        backgroundColor = .secondarySystemBackground
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
